b76f5d3fde1f71e35260ab1a6288d5999ffbfa98
Sannan47 <sannanshakeel04@gmail.com>
2026-03-06 14:47:35 +0500

    added express server, session and register

--- task3/server.js
+++ task3/server.js
@@ -1,4 +1,7 @@
+const express = require("express");
+const session = require("express-session");
 const mongoose = require("mongoose");
+const User = require("./User");
 
 mongoose.connect("mongodb://localhost:27017/studentDB")
 .then(() => {
@@ -6,4 +9,28 @@ mongoose.connect("mongodb://localhost:27017/studentDB")
 })
 .catch((err) => {
     console.log(err);
+});
+
+const app = express();
+
+app.use(express.json());
+
+app.use(session({
+    secret: "secretKey",
+    resave: false,
+    saveUninitialized: true
+}));
+
+
+app.post("/register", async (req, res) => {
+
+    console.log(req.body);
+
+    const { username, password } = req.body;
+
+    const user = new User(username, password);
+
+    const result = await user.register();
+
+    res.send(result);
 });
